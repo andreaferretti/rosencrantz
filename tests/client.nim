6b1898e96a4b7e4a43bd7005f6aa23d3157d3dc0
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-03-22 08:34:59 +0100

    Added test

--- tests/client.nim
+++ tests/client.nim
@@ -1,4 +1,4 @@
-import unittest, httpclient
+import unittest, httpclient, strtabs, strutils
 
 const baseUrl = "http://localhost:8080"
 
@@ -6,3 +6,6 @@ suite "basic functionality":
   test "simple text":
     let resp = getContent(baseUrl & "/benchmark/text")
     check resp == "Hello, World!"
+  test "simple text content type":
+    let resp = get(baseUrl & "/benchmark/text")
+    check resp.headers["Content-Type"].startsWith("text/plain")
