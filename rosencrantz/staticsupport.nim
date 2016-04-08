908d2553f09bd4229c78dc45ceb90569957b42a9
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-04-08 19:45:15 +0200

    Fixed mime type for static files

--- rosencrantz/staticsupport.nim
+++ rosencrantz/staticsupport.nim
@@ -1,14 +1,20 @@
-import asynchttpserver, asyncdispatch, asyncfile, strtabs, os
+import asynchttpserver, asyncdispatch, asyncfile, strtabs, os, mimetypes
 import rosencrantz/core, rosencrantz/handlers
+
+let mime = newMimetypes()
 
 
 proc file*(path: string): Handler =
+  let
+    (_, _, ext) = splitFile(path)
+    mimeType = mime.getMimetype(ext)
+
   proc h(req: ref Request, ctx: Context): Future[Context] {.async.} =
     if fileExists(path):
       let f = openAsync(path)
       let content = await f.readAll
       close(f)
-      var hs = {"Content-Type": "text/plain"}.newStringTable
+      var hs = {"Content-Type": mimeType}.newStringTable
       # Should traverse in reverse order
       for h in ctx.headers:
         hs[h.k] = h.v
@@ -27,10 +33,13 @@ proc dir*(path: string): Handler =
       fileName = p[ctx.position .. p.high]
       completeFileName = path / fileName
     if fileExists(completeFileName):
-      let f = openAsync(completeFileName)
+      let
+        (_, _, ext) = splitFile(completeFileName)
+        mimeType = mime.getMimetype(ext)
+        f = openAsync(completeFileName)
       let content = await f.readAll
       close(f)
-      var hs = {"Content-Type": "text/plain"}.newStringTable
+      var hs = {"Content-Type": mimeType}.newStringTable
       # Should traverse in reverse order
       for h in ctx.headers:
         hs[h.k] = h.v
