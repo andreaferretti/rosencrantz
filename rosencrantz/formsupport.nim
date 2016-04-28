6928be3b94147d96c0b7e047c80bde68eccd06ba
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-04-28 19:42:53 +0200

    Added handler for forms with repeated parameters

--- rosencrantz/formsupport.nim
+++ rosencrantz/formsupport.nim
@@ -1,4 +1,4 @@
-import strtabs, asynchttpserver, asyncdispatch
+import strtabs, tables, asynchttpserver, asyncdispatch
 import rosencrantz/core, rosencrantz/util
 
 
@@ -7,6 +7,19 @@ proc formBody*(p: proc(s: StringTableRef): Handler): Handler =
     var s: StringTableRef
     try:
       s = req.body.parseUrlEncoded
+    except:
+      return ctx.reject()
+    let handler = p(s)
+    let newCtx = await handler(req, ctx)
+    return newCtx
+
+  return h
+
+proc formBody*(p: proc(s: TableRef[string, seq[string]]): Handler): Handler =
+  proc h(req: ref Request, ctx: Context): Future[Context] {.async.} =
+    var s: TableRef[string, seq[string]]
+    try:
+      s = req.body.parseUrlEncodedMulti
     except:
       return ctx.reject()
     let handler = p(s)
