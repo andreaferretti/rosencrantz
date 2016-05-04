7ea46e3b674e9d86d752a3155798d8cbc39a2c48
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-05-04 20:21:25 +0200

    Fixed capturing in scope template

--- rosencrantz/custom.nim
+++ rosencrantz/custom.nim
@@ -1,16 +1,24 @@
 import asyncHttpServer, asyncDispatch, macros
 import rosencrantz/core
 
-template scope*(body: untyped): untyped =
-  proc inner: auto {.gensym.} = body
-  inner()
-
 proc getRequest*(p: proc(req: ref Request): Handler): Handler =
   proc h(req: ref Request, ctx: Context): Future[Context] {.async.} =
     let handler = p(req)
     return (await handler(req, ctx))
 
   return h
+
+proc before*(p: proc(): Handler): Handler =
+  proc h(req: ref Request, ctx: Context): Future[Context] {.async.} =
+    let h1 = p()
+    return await h1(req, ctx)
+
+  return h
+
+template scope*(body: untyped): untyped =
+  proc inner: auto {.gensym.} = body
+
+  before(inner)
 
 macro makeHandler*(body: untyped): untyped =
   template inner(body: untyped): untyped  {.dirty.} =
